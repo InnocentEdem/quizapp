2161769baab7e1293f97d736bb7e6d72fc1b9e0c
InnocentEdem <innocent.kuwornu@amalitech.org>
2021-04-15 16:22:22 +0000

    complete assignment without customization

--- game.js
+++ game.js
@@ -2,36 +2,59 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"))
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
-const progressBarFull = document.getElementById("progressBarFull")
+const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestion = [];
-let questions=[];
-fetch("questions.json"). then( res => {
-    return res.json();
-}).then(loadedQuestions => {
-    questions = loadedQuestions;
-    startGame();
-});
+let availableQuestions = [];
+ let questions=[];
+
+fetch("https://opentdb.com/api.php?amount=10&category=18")
+    .then( res => {
+        return res.json();
+        }).then(loadedQuestions => {
+            questions = loadedQuestions.results.map(loadedQuestion =>{
+            const formattedQuestion = {
+            question: loadedQuestion.question
+             };
+              
+             const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 3) +1;
+            answerChoices.splice(
+            formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer
+             );
+
+            answerChoices.forEach((choice, index) =>{
+            formattedQuestion["choice" + (index + 1)] = choice;
+             });
+             return formattedQuestion;
+            });
+            
+
+            startGame();
+        });
 
  
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 5;
 
 startGame = ()=>{
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion(); 
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
 };
 
 getNewQuestion=function(){
     if(availableQuestions === 0 || questionCounter===MAX_QUESTIONS){
         localStorage.setItem("mostRecentScore", score) 
         return window.location.assign("/end.html");
-        console.log(score)
+      
     }
 
     questionCounter++;
@@ -46,8 +69,14 @@ getNewQuestion=function(){
     choices.forEach(choice => {
     const number = choice.dataset["number"];
     thisChoices = "choice"+number;
-    console.log(thisChoices);
-    choice.innerText = currentQuestion[thisChoices];});
+    let blankOption = "  ";
+    if (!currentQuestion[thisChoices]){
+        choice.innerText = blankOption;  
+    }
+    else {
+    choice.innerText = currentQuestion[thisChoices];
+         }
+    });
     availableQuestions.splice(questionIndex, 1);
     acceptingAnswers = true;
 };
@@ -70,12 +99,12 @@ choices.forEach(choice => {
         setTimeout(()=>{
 
             selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();}, 1000)
+            getNewQuestion();}, 1000);
 
 
-    })
-})
-incrementScore = num => {
-    
-};
-
+    });
+});
+ incrementScore = num =>{
+     score = score + num;
+     scoreText.innerHTML = score;
+ };
